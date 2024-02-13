e1a528a705568587a3a9efcd22698343de884c84
Alexey-Home <99196626+Alexey-Home@users.noreply.github.com>
2024-02-13 21:09:00 +0300

    created a player class

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ def main():
 
     screen = pygame.display.set_mode((1024, 768))
     color_bkg = (230, 234, 240)
-    # player = pygame.image.load("images/man1.jpg")
     player = Player()
 
     pygame.display.flip()
@@ -16,34 +15,13 @@ def main():
     while running:
         screen.fill(color_bkg)
         screen.blit(player.image, (player.x, player.y))
-        print(player.x)
 
         keys = pygame.key.get_pressed()
 
+        player.run(keys)
+        player.jump(keys)
 
-        if keys[pygame.K_RIGHT] and player.x < 1000:
-            player.speed_increase()
-            player.x += player.speed
-        elif keys[pygame.K_LEFT] and player.x > 24:
-            player.speed_increase()
-            player.x -= player.speed
-        else:
-            player.speed_reset()
 
-        if not player.is_jump:
-            if keys[pygame.K_SPACE]:
-                player.is_jump = True
-        else:
-            if player.jump_count >= -player.jump_high:
-                if player.jump_count > 0:
-                    player.y -= (player.jump_count ** 2) / 2
-                else:
-                    player.y += (player.jump_count ** 2) / 2
-                player.jump_count -= 1
-            else:
-                player.is_jump = False
-                player.jump_count = player.jump_high
-        
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
--- player.py
+++ player.py
@@ -8,16 +8,48 @@ class Player:
         self.y = 700
         self.speed = 0.001
         self.image = pygame.image.load("images/man1.jpg")
-        self.jump_count = 7
-        self.jump_high = 7
+        self.jump_count = 100
+        self.jump_high = 100
         self.y_begin_jump = self.y
-        self.is_jump = True
+        self.is_jump = False
+        self.single_jump = True
 
     def speed_increase(self):
         self.speed += 0.001
 
     def speed_reset(self):
         self.speed = 0.001
+
+    def run(self, keys):
+        if keys[pygame.K_RIGHT] and keys[pygame.K_LEFT]:
+            self.speed_reset()
+        elif keys[pygame.K_RIGHT] and self.x < 1000:
+            self.speed_increase()
+            self.x += self.speed
+        elif keys[pygame.K_LEFT] and self.x > 24:
+            self.speed_increase()
+            self.x -= self.speed
+        else:
+            self.speed_reset()
+
+
+    def jump(self, keys):
+
+        if not self.is_jump:
+            if keys[pygame.K_SPACE]:
+                self.is_jump = True
+            else:
+                self.single_jump = False
+        else:
+            if self.jump_count >= -self.jump_high:
+                if self.jump_count > 0:
+                    self.y -= (self.jump_count ** 2) / 5000
+                else:
+                    self.y += (self.jump_count ** 2) / 5000
+                self.jump_count -= 1
+            else:
+                self.is_jump = False
+                self.jump_count = self.jump_high
 
 
 
