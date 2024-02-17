acb3f84ae2f850536b1c78913d17096b07a08818
Alexey-Home <99196626+Alexey-Home@users.noreply.github.com>
2024-02-17 21:42:09 +0300

    developed collisions

--- main.py
+++ main.py
@@ -1,34 +1,68 @@
 import pygame
 from player import Player
 
+
 def main():
 
-    pygame.init()
+    width = 1024
+    height = 768
 
-    screen = pygame.display.set_mode((1024, 768))
+    pygame.init()
+    clock = pygame.time.Clock()
+    screen = pygame.display.set_mode((width, height))
     color_bkg = (230, 234, 240)
     player = Player()
 
     pygame.display.flip()
-    running = True
+    gameplay = True
 
-    while running:
+    rect1 = pygame.Surface((200, 30))
+    rect1.fill("Grey")
+    rect1_col = rect1.get_rect(topleft=(0, 720))
+
+    rect2 = pygame.Surface((200, 30))
+    rect2.fill("Grey")
+    rect2_col = rect2.get_rect(topleft=(250, 720))
+
+    rect3 = pygame.Surface((200, 100))
+    rect3.fill("Grey")
+    rect3_col = rect3.get_rect(topleft=(250, 550))
+
+
+    object_collisions = [rect1_col, rect2_col, rect3_col]
+
+
+    while gameplay:
         screen.fill(color_bkg)
+        screen.blit(rect1, (0, 720))
+        screen.blit(rect2, (250, 720))
+        screen.blit(rect3, (250, 550))
         screen.blit(player.image, (player.x, player.y))
 
         keys = pygame.key.get_pressed()
-
-        player.run(keys)
-        player.jump(keys)
-
-
+        #player_col = player.image.get_rect(topleft=(player.x, player.y))
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                running = False
+                gameplay = False
                 pygame.quit()
+            elif event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE and player.is_col:
+                player.is_jump = True
+            elif event.type == pygame.KEYDOWN and (event.key == pygame.K_RIGHT or event.key == pygame.K_LEFT):
+                player.is_run = True
+            elif event.type == pygame.KEYUP and (event.key == pygame.K_RIGHT or event.key == pygame.K_LEFT):
+                player.is_run = False
+
+        player.physics(object_collisions)
+        player.run(keys)
+        player.jump()
+
+        if player.y > height:
+            gameplay = False
 
         pygame.display.update()
+        clock.tick(60)
+
 
 
 
--- player.py
+++ player.py
@@ -4,52 +4,82 @@ import pygame
 class Player:
 
     def __init__(self):
-        self.x = 30
-        self.y = 700
-        self.speed = 0.001
+        self.x = 50
+        self.y = 400
+        self.speed = 0
         self.image = pygame.image.load("images/man1.jpg")
-        self.jump_count = 100
-        self.jump_high = 100
+        self.jump_count = 15
+        self.racing = 0.1
+        self.jump_high = self.jump_count
         self.y_begin_jump = self.y
         self.is_jump = False
-        self.single_jump = True
+        self.single_jump = False
+        self.is_run = True
+        self.direction_of_move = None
+        self.collision = self.image.get_rect(topleft=(self.x, self.y))
+        self.is_col = False
 
     def speed_increase(self):
-        self.speed += 0.001
+        """Увелечение скорости игрока"""
+        self.speed += self.racing
 
     def speed_reset(self):
-        self.speed = 0.001
+        """Сброс скорости игрока"""
+        self.speed = 0
+        self.racing = 0.1
+
+    def speed_reduction(self):
+        """Уменьшение скорости игрока"""
+        self.racing += 0.01
+        self.speed -= self.racing
 
     def run(self, keys):
-        if keys[pygame.K_RIGHT] and keys[pygame.K_LEFT]:
-            self.speed_reset()
-        elif keys[pygame.K_RIGHT] and self.x < 1000:
-            self.speed_increase()
-            self.x += self.speed
-        elif keys[pygame.K_LEFT] and self.x > 24:
-            self.speed_increase()
-            self.x -= self.speed
-        else:
-            self.speed_reset()
-
-
-    def jump(self, keys):
-
-        if not self.is_jump:
-            if keys[pygame.K_SPACE]:
-                self.is_jump = True
+        """Бег игрока влево и вправо"""
+        if self.is_run:
+            if keys[pygame.K_RIGHT] and self.x < 1000 and self.is_col:
+                self.direction_of_move = "right"
+                self.speed_increase()
+                self.x += self.speed
+            elif keys[pygame.K_LEFT] and self.x > 24 and self.is_col:
+                self.direction_of_move = "left"
+                self.speed_increase()
+                self.x -= self.speed
             else:
-                self.single_jump = False
+                self.speed_reset()
         else:
-            if self.jump_count >= -self.jump_high:
-                if self.jump_count > 0:
-                    self.y -= (self.jump_count ** 2) / 5000
-                else:
-                    self.y += (self.jump_count ** 2) / 5000
-                self.jump_count -= 1
+            if self.direction_of_move == "right" and self.speed > 0 and self.x < 1000:
+                self.speed_reduction()
+                self.x += self.speed
+            elif self.direction_of_move == "left" and self.speed > 0 and self.x > 24:
+                self.speed_reduction()
+                self.x -= self.speed
+            else:
+                self.speed_reset()
+
+
+    def jump(self):
+        """Прыжок игрока"""
+        if self.is_jump and self.is_col:
+            if self.jump_count >= 0:
+                self.y -= (self.jump_count ** 2) / 10
             else:
                 self.is_jump = False
                 self.jump_count = self.jump_high
+            self.jump_count -= 1
 
+
+    def physics(self, collisions):
+        """Притяжение игрока"""
+        self.collision = self.image.get_rect(topleft=(self.x, self.y))
+        for num, col in enumerate(collisions):
+            if self.collision.colliderect(col):
+                self.is_col = True
+                break
+            else:
+                continue
+        else:
+            if not self.is_jump:
+                self.y += 5
+                self.is_col = False
 
 
